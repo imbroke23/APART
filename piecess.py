60dbb3e7e92a280424f36bcde8cdab291bf110fa
imbroke23 <84753750+imbroke23@users.noreply.github.com>
2022-12-31 23:18:44 -0500

    Add files via upload

--- piecess.py
+++ piecess.py
@@ -1,9 +1,13 @@
-import requests
+#import requests
 
-document_id = "1JCFNn5yj_-flsGLzeqy6ETYGV_f5pJl2YpEKNabiLic"  # Replace with the ID of your Google Doc
-export_format = "txt"  # Replace with the desired export format (e.g. "html", "txt", etc.)
-api_key = "AIzaSyA0rvC9zZIA1Rp_G0NswkihEqkhK2czeL8"  # Replace with your API key
+#document_id = "1JCFNn5yj_-flsGLzeqy6ETYGV_f5pJl2YpEKNabiLic"  # Replace with the ID of your Google Doc
+#export_format = "txt"  # Replace with the desired export format (e.g. "html", "txt", etc.)
+#api_key = "AIzaSyA0rvC9zZIA1Rp_G0NswkihEqkhK2czeL8"  # Replace with your API key
 
-url = f"https://docs.google.com/document/d/1JCFNn5yj_-flsGLzeqy6ETYGV_f5pJl2YpEKNabiLic/export?format=txt&key=AIzaSyA0rvC9zZIA1Rp_G0NswkihEqkhK2czeL8"
-response = requests.get(url)
-document_contents = response.text
+#url = f"https://docs.google.com/document/d/1JCFNn5yj_-flsGLzeqy6ETYGV_f5pJl2YpEKNabiLic/export?format=txt&key=AIzaSyA0rvC9zZIA1Rp_G0NswkihEqkhK2czeL8"
+#response = requests.get(url)
+#document_contents = response.text
+
+for i in range(99, 251):
+    print(f'<li><a href="{i}.html">{i}. x</a></li>')
+
